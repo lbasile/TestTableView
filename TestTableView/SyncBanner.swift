67fe02cfacf8cc1c456bd48e4a90ac7123cd471b
Louis Basile <louis.basile@imanage.com>
2018-05-06 13:44:52 -0500

    initial commit

---
+++ TestTableView/SyncBanner.swift
@@ -0,0 +1,74 @@
+//
+//  SyncBanner.swift
+//  TestTableView
+//
+//  Created by Louis Basile on 5/3/18.
+//  Copyright © 2018 Louis Basile. All rights reserved.
+//
+
+import UIKit
+
+public class SyncBanner: UIButton {
+    weak var weakViewController: UIViewController?
+    weak var weakScrollView: UIScrollView?
+    
+    var noHeightConstraint: NSLayoutConstraint!
+    var heightConstraint: NSLayoutConstraint!
+    var height: CGFloat { return 40.0 }
+    
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+    
+    public required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setup()
+    }
+    
+    private func setup() {
+        translatesAutoresizingMaskIntoConstraints = false
+        backgroundColor = UIColor(red:0.86, green:0.93, blue:0.9, alpha:1)
+    }
+    
+    public func attach(to: UIViewController, above: UIScrollView?) {
+        weakViewController = to
+        weakScrollView = above
+        
+        to.view.addSubview(self)
+        
+        topAnchor.constraint(equalTo: to.view.safeAreaLayoutGuide.topAnchor).isActive = true
+        leadingAnchor.constraint(equalTo: to.view.safeAreaLayoutGuide.leadingAnchor).isActive = true
+        trailingAnchor.constraint(equalTo: to.view.safeAreaLayoutGuide.trailingAnchor).isActive = true
+        
+        noHeightConstraint = heightAnchor.constraint(equalToConstant: 0)
+        noHeightConstraint.isActive = true
+        
+        heightConstraint = heightAnchor.constraint(equalToConstant: height)
+        heightConstraint.isActive = false
+    }
+    
+    public func show() {
+        isEnabled = true
+        noHeightConstraint.isActive = false
+        heightConstraint.isActive = true
+        animateHeight(to: height)
+    }
+    
+    public func hide() {
+        isEnabled = false
+        heightConstraint.isActive = false
+        noHeightConstraint.isActive = true
+        animateHeight(to: 0)
+    }
+    
+    private func animateHeight(to height: CGFloat) {
+//        layoutIfNeeded()
+//        noHeightConstraint.constant = height
+        UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 0, options: [], animations: {
+            self.layoutIfNeeded()
+            self.weakScrollView?.contentInset = UIEdgeInsetsMake(height, 0, 0, 0)
+            self.weakScrollView?.contentOffset = CGPoint(x: 0, y: -height)
+        }, completion: nil)
+    }
+}
--- TestTableView/ViewController.swift
+++ TestTableView/ViewController.swift
@@ -10,16 +10,122 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    var selectAllButton: UIBarButtonItem?
+    var syncBanner: SyncBanner!
+    
+    var selectAllTitle: String {
+        if isAllSelected() {
+            return "Deselect All"
+        }
+        return "Select All"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        setupToolbar()
+        setupTableView()
+        setupSyncBanner()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func setupSyncBanner() {
+        syncBanner = SyncBanner()
+        syncBanner.attach(to: self, above: tableView)
     }
+    
+    func setupTableView() {
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.allowsMultipleSelectionDuringEditing = true
+        tableView.tableFooterView = UIView()
+        tableView.separatorStyle = .none
+        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+    }
+    
+    func setupToolbar() {
+        selectAllButton = UIBarButtonItem(title: selectAllTitle, style: .plain, target: self, action: #selector(toggleSelectAll))
+        navigationController?.setToolbarHidden(false, animated: false)
+        showToolbarItems()
+    }
+    
+    func showToolbarItems() {
+        let flexSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: self, action: nil)
+        let editButton = UIBarButtonItem(title: "Select", style: .plain, target: self, action: #selector(toggleEdit))
+        
+        if tableView.isEditing {
+            editButton.title = "Cancel"
+            setToolbarItems([flexSpace, selectAllButton!, editButton], animated: true)
+        } else {
+            editButton.title = "Select"
+            setToolbarItems([flexSpace, editButton], animated: true)
+        }
+    }
+    
+    @objc func toggleEdit() {
+        tableView.setEditing(!tableView.isEditing, animated: true)
+        
+        if tableView.isEditing {
+            syncBanner.show()
+        } else {
+            syncBanner.hide()
+        }
+        
+        showToolbarItems()
+    }
+    
+    @objc func toggleSelectAll() {
+        if isAllSelected() {
+            deselectAll()
+        } else {
+            selectAll()
+        }
+        selectAllButton?.title = selectAllTitle
+    }
+    
+    func isAllSelected() -> Bool {
+        return tableView.indexPathsForSelectedRows?.count == tableView.numberOfRows(inSection: 0)
+    }
+    
+    func selectAll() {
+        for row in 0..<tableView.numberOfRows(inSection: 0) {
+            tableView.selectRow(at: IndexPath(row: row, section: 0), animated: true, scrollPosition: .none)
+        }
+    }
+    
+    func deselectAll() {
+        for row in 0..<tableView.numberOfRows(inSection: 0) {
+            tableView.deselectRow(at: IndexPath(row: row, section: 0), animated: true)
+        }
+    }
+}
 
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 100
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        let numRows = tableView.numberOfRows(inSection: indexPath.section)
+        cell.backgroundColor = UIColor(hue: (CGFloat(indexPath.row)/CGFloat(numRows)), saturation: 0.6, brightness: 1, alpha: 1)
+        cell.textLabel?.text = "Here is some repeating text."
+//        cell.backgroundColor = UIColor(white: 0, alpha: CGFloat(indexPath.row)/CGFloat(numRows))
+        
+        let selectedView = UIView()
+        selectedView.backgroundColor = UIColor(white: 1, alpha: 0.3)
+        cell.selectedBackgroundView? = selectedView
+        
+        return cell
+    }
+}
 
+extension ViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        selectAllButton?.title = selectAllTitle
+    }
+    func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
+        selectAllButton?.title = selectAllTitle
+    }
 }
 
